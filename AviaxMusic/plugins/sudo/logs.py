fd2c78f6446a77a74843a076024a591317f5ffcf
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-22 17:06:01 +0530

    Update logs.py

--- AviaxMusic/plugins/sudo/logs.py
+++ AviaxMusic/plugins/sudo/logs.py
@@ -1,17 +1,8 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from AloneMusic import app
-from AloneMusic.utils.database import add_served_chat
+from AviaxMusic import app
+from AviaxMusic.utils.database import add_served_chat
 from config import LOGGER_ID as LOG_GROUP_ID
 
 
