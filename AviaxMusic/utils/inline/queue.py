aeaaade30854df683ff2a0ee79bb37aaa4601f91
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-22 13:18:19 +0530

    Update queue.py

--- AviaxMusic/utils/inline/queue.py
+++ AviaxMusic/utils/inline/queue.py
@@ -1,5 +1,6 @@
+import config
 from typing import Union
-
+from config import OWNER_ID
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -65,10 +66,15 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
+     #   [
+            #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            #InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            #InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        #],
         [
             InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
+                text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=config.OWNER_ID,
             ),
         ],
     ]
