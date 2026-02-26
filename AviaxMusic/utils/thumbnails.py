431aeaafd51f038da20bf5f6993554b3d3501bb4
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-26 12:37:22 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -2,29 +2,31 @@ import random
 
 # 🔥 Tumhare diye hue images
 RANDOM_THUMBS = [
-      "https://files.catbox.moe/a6hzmb.jpg",
-    "https://files.catbox.moe/ohezme.jpg",
-    "https://files.catbox.moe/spylio.jpg",
-    "https://files.catbox.moe/5go4t6.jpg",
-    "https://files.catbox.moe/ikxb96.jpg",
-    
+    "https://files.catbox.moe/ikxb96.jpg",
+    "https://files.catbox.moe/dqxsjh.jpg",
+    "https://files.catbox.moe/lnaqxk.jpg",
+    "https://files.catbox.moe/0qzssp.jpg",
+    "https://files.catbox.moe/lz57vi.jpg",
+    "https://files.catbox.moe/x1fcb6.jpg",
+    "https://files.catbox.moe/32ghsc.jpg",
+    "https://files.catbox.moe/tm8vmd.jpg",
 ]
 
 _last_thumb = None
 
-async def gen_thumb(videoid=None):  # videoid ignore
-    global _last_thumb
+async def gen_thumb(videoid=None):  # videoid ignore
+    global _last_thumb
 
-    try:
-        choice = random.choice(RANDOM_THUMBS)
+    try:
+        choice = random.choice(RANDOM_THUMBS)
 
-        # ❌ same image repeat na ho
-        while choice == _last_thumb and len(RANDOM_THUMBS) > 1:
-            choice = random.choice(RANDOM_THUMBS)
+        # ❌ same image repeat na ho
+        while choice == _last_thumb and len(RANDOM_THUMBS) > 1:
+            choice = random.choice(RANDOM_THUMBS)
 
-        _last_thumb = choice
-        return choice
+        _last_thumb = choice
+        return choice
 
-    except Exception as e:
-        print(e)
-        return RANDOM_THUMBS[0]
+    except Exception as e:
+        print(e)
+        return RANDOM_THUMBS[0]
